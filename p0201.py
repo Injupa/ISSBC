893ecb02d0351f59a5ef7483b47602bf185a342b
Juan Parejo <i42papaj@uco.es>
2018-02-28 15:06:24 +0100

    Add files via upload

---
+++ p0201.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 15:04:46 2018
+
+@author: juanp
+"""
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        
+        lcd = QtGui.QLCDNumber(self)
+        sld = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+        
+        self.setGeometry(300, 300, 250, 100)
+        self.setWindowTitle('Signal & slot')
+        self.show()
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+main()
